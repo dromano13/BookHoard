a70dfe33dd8bbf5199311996a1cf27ecbbe640bc
Dan Romano <Dan_Romano@glic.com>
2020-06-17 12:27:56 -0400

    auth controller started

---
+++ controllers/auth.js
@@ -0,0 +1,34 @@
+require('dotenv').config()
+
+const User = require('../models').User;
+
+const bcrypt = require('bcryptjs');
+
+const renderSignup = (req, res) => {
+    res.render('users/signup.ejs');
+}
+
+const signup = (req, res) => {
+    bcrypt.genSalt(10, (err, salt) => {
+        if (err) return res.status(500).json(err);
+    
+        bcrypt.hash(req.body.password, salt, (err, hashedPwd) => {
+            if (err) return res.status(500).json(err);
+            req.body.password = hashedPwd;
+            
+            User.create(req.body)
+            .then(newUser => {
+                res.redirect(`/users/profile/${newUser.id}`);
+            })
+            .catch(err => {
+                console.log(err);
+                res.send(`err ${err}`);
+            })
+        });
+    });
+}
+
+module.exports = {
+    renderSignup,
+    signup
+}
---
+++ server.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+
+
+const express = require('express');
+
+const bodyParser = require('body-parser');
+
+const methodOverride = require('method-override');
+
+const app = express();
+
+const routes = require('./routes');
+
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(methodOverride('_method'));
+app.use(express.static(__dirname + '/public'));
+
+
+app.get('/', (req, res) => {
+    res.render('users/index.ejs')
+})
+
+
+app.use('/users', routes.users);
+app.use('/auth', routes.auth);
+
+app.listen(process.env.PORT, () => {
+    console.log('I am listening');
+});
